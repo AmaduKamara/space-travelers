492ec5045be6e316eaf8f8e34cb4915a79308bba
Amkam <amaduskamara@gmail.com>
2022-01-26 08:38:15 +0000

    Add sections for My Missions and My Rockets in My profile page

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -6,8 +6,12 @@ function MyProfile() {
   const filteredRockets = rockets.filter((rocket) => rocket.reserved);
 
   return (
-    <div className="container mx-auto">
-      <div>
+    <div className="container mx-auto flex">
+      <div className="w-1/2 border mx-5 mt-10 p-5">
+        <h1>My Mission</h1>
+        <p>Place your reserved missions here...</p>
+      </div>
+      <div className="w-1/2 border mx-5 mt-10 p-5">
         <h1>My Rockets</h1>
         {filteredRockets.map((rocket) => (
           <div key={rocket.id}>{rocket.rocket_name}</div>
