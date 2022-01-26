437fcf7be77505f86de759d81bce41a62edb107c
Amkam <amaduskamara@gmail.com>
2022-01-26 13:49:30 +0000

    Styled My Profile and active link on the navigation

--- src/pages/MyProfile.jsx
+++ src/pages/MyProfile.jsx
@@ -7,14 +7,16 @@ function MyProfile() {
 
   return (
     <div className="container mx-auto flex">
-      <div className="w-1/2 border mx-5 mt-10 p-5">
-        <h1>My Mission</h1>
+      <div className="w-1/2 mx-5 mt-10 p-5">
+        <h1 className="mb-4 text-xl font-semibold">My Mission</h1>
         <p>Place your reserved missions here...</p>
       </div>
-      <div className="w-1/2 border mx-5 mt-10 p-5">
-        <h1>My Rockets</h1>
+      <div className="w-1/2 mx-5 mt-10 pt-5">
+        <h1 className="mb-4 text-xl font-semibold">My Rockets</h1>
         {filteredRockets.map((rocket) => (
-          <div key={rocket.id}>{rocket.rocket_name}</div>
+          <div key={rocket.id} className="border px-6 py-5">
+            {rocket.rocket_name}
+          </div>
         ))}
       </div>
     </div>
